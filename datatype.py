4b28d81cc7cebf9c930e4f5bde4d632989bcb698
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-13 14:11:03 +0545

    update

---
+++ datatype.py
@@ -0,0 +1,10 @@
+# This programm is just for the data types.
+a = 5        # a is an integer
+
+b = 6.9      # b is a floating point number
+
+c = "Raman"  #  c is a string
+
+d = False    #  d is a boolean variable 
+
+e = None     #  e is a none type varaible
