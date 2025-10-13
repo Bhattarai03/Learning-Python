1f0f0844e3d371a90325e5086dc9b7829412bbe2
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-13 13:50:03 +0545

    update

--- program3.py
+++ program3.py
@@ -5,5 +5,5 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("If you’re hearing this, congrats! You just ran something cool")
+engine.say(" If you’re hearing this, congrats! You just ran something cool")
 engine.runAndWait()
