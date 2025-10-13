2dccb749c0db17b4547379dc40bd53338ef4a02d
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-13 13:09:07 +0545

    update

--- program4.py
+++ program4.py
@@ -1,9 +1,10 @@
-# Write a python program to print the content of a current directory using the os module .
+# Write a python program to print the content of a Specific directory using the os module .
 
 import os
+directory_path ='c:/'
 
-# List all entries in the current directory
-entries = os.listdir()
+# List all entries in the specific directory
+entries = os.listdir(directory_path)
 
 # Print each entry
 for entry in entries:
