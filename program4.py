29751f249654cdb5b17a1aedc8850ada79cf1002
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-13 12:58:30 +0545

    update

---
+++ program4.py
@@ -0,0 +1,10 @@
+# Write a python program to print the content of a directory using the os module .
+
+import os
+
+# List all entries in the current directory
+entries = os.listdir()
+
+# Print each entry
+for entry in entries:
+    print(entry)
