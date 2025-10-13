4c7e5dfade971fa0200f99d2deaa1cdfbac8f086
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-13 14:47:36 +0545

    update

---
+++ Operator.py
@@ -0,0 +1,34 @@
+# Arithmetic Operator
+
+a = 7
+b = 5
+c=a+b
+print(c)
+
+# Assignment Operator
+
+a = 9-3  # Assign 9-3 in a
+print(a)
+b =  9
+b += 6   # Increment the value of b by 6 and then assign it to  b
+print(b)
+c=15
+c -= 9   # Decrement in th value of c by   9 and then assign it to c
+print(c)
+
+# Comparision Operator
+
+f = 7 != 6
+g = 6 == 5
+h = 7 >= 6
+i = 9 <= 4
+
+print(f)
+print (g)
+print(h)
+print (i)
+
+# Logical operator
+''' and gate (display true when both are true otherwise false)
+    or gate   (display true when any one or both of them are  true otherwise false)
+    not gate (display true when input us false and viceversa)'''
