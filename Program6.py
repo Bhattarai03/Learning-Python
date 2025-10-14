146a5819dba5fe6e76db384a667859863026ea65
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-14 11:54:01 +0545

    update

---
+++ Program6.py
@@ -0,0 +1,11 @@
+# Write a python to find remainder when a number is divided by z.
+
+a=int(input("Enter number you want to divide by z :"))
+z=int(input("Enter z :"))
+
+print("  Num is ",a)
+print("  Z is ",z)
+
+
+t=a%z
+print("Remainder is ",t)
