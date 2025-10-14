f717082f5f2dfea302adb2ae3eb6cb1ac070c772
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-14 09:18:55 +0545

    Add files via upload

---
+++ type.py
@@ -0,0 +1,37 @@
+# Use type function for changing the type of valid variable
+# String to float conversion
+a = "35.32"
+b = float(a)# a is str  but the type should float
+t = type(b)
+print(t)
+
+# Integer to float conversion
+
+f=35
+g=float(f)
+h=type(g)
+print(h)
+
+# integer to string conversion
+i = 45
+e=str(i)
+v=type(e)
+print(v)
+
+# String to integer conversion
+z="35"
+y=int(z)
+s=type(y)
+print(s)
+
+#Float to integer conversion
+m=34.00
+x=int(m)
+w=type(x)
+print(w)
+
+# Float  to string conversion
+l=34.56
+n=str(l)
+v=type(l)
+print(v)
