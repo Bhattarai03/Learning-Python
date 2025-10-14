5ebecbdef963901060e5599f5a5f74e56995ed76
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-14 10:41:46 +0545

    update

---
+++ Input.py
@@ -0,0 +1,23 @@
+# To simply join the integer i.e. 5+4 =54
+a=input("enter 1st number :")
+
+b=input("enter 2nd number :")
+
+print ("number 1 is ",a)
+print ("number 2 is ",b)
+print("The sum is ",a + b)
+# Example :
+'''
+1 number :3
+2 number :4
+output:34
+'''
+# To actually add the two number we need to add int before input syntax
+# i.e. 5+4=9
+c=int(input("Enter 1st number :"))
+d=int(input("Enter 2nd number:"))
+
+print("number 1 is ",c)
+print("nNumber 2 is ",d)
+print("sum is :",c+d)
+
