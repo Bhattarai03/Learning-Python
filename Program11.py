e6d680623cf4309d7338c17f1e47a3010e6b8311
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-14 17:06:16 +0545

    update

--- Program11.py
+++ Program11.py
@@ -3,8 +3,9 @@
 a=str(input("Enter a name :"))
 b=str(input("Enter a date :"))
 
+d=a.title()
 c=''' Dear Name
     YOu are  one of the best player in the world
     date
     '''
-print(c.replace("Name",a).replace("date",b))
+print(c.replace("Name",d).replace("date",b))
