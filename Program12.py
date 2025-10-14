8074266e8396c95d13f6c16ee85172e371bb925e
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-14 17:57:01 +0545

    update

--- Program12.py
+++ Program12.py
@@ -1,4 +1,8 @@
 # Write a program to detect double space in the string.
+
 a=str(input("Enter a sentences:"))
 b=a.find("  ")
 print(b)
+
+print(a.replace("  "," "))
+  # This line of code is for replacing double space into a single space.
