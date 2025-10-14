874feb92aaf0a43c65af8a0fc84b4e69d554b8cc
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-14 16:36:46 +0545

    update

---
+++ Program10.py
@@ -0,0 +1,5 @@
+# Write a python program to display a user entered name followed by Good Afternoon using input() function.
+
+a=str(input("Enter a name :"))
+t=a.capitalize()
+print("Good Afternoon",t)
