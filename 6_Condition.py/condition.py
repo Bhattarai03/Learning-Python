f0ce76fe514241ead77f9c3ff72e231f3a08f6d5
Raman Raj Bhattarai <bhattarai.ramanraj2006@gmail.com>
2025-10-17 18:31:10 +0545

    Update

---
+++ 6_Condition.py/condition.py
@@ -0,0 +1,18 @@
+a=int(input("enter marks"))
+ 
+#  If statement 1 and it is independent if statement.
+if(a%2==0):
+    print("The number is even")
+
+if(a>18):
+    print("you are the handsome student in the class")
+    print("You are the smartest guy in the class.")
+
+elif(a<0):
+    print("You are entering invalid marks")
+
+elif(a==0):
+    print("You are unique")
+
+else:
+    print("you are the  dumbest in the class")
